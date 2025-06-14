3110ec56a61b14244d81446996d82279e63f22be
quangbach <167604957+quangbach27@users.noreply.github.com>
2025-06-14 16:24:25 +0700

    update gitignore

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,18 +1,26 @@
+-- Drop in correct order (hours depends on dates)
+DROP TABLE IF EXISTS hours;
+DROP TABLE IF EXISTS dates;
+DROP TYPE IF EXISTS availability_status;
+
+-- Recreate ENUM type
 CREATE TYPE availability_status AS ENUM (
     'available',
     'not_available',
     'training_scheduled'
 );
 
+-- Create dates table
 CREATE TABLE dates (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    uuid UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     date DATE NOT NULL UNIQUE
 );
 
+-- Create hours table
 CREATE TABLE hours (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    uuid UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     hour TIMESTAMP NOT NULL,
     availability availability_status NOT NULL,
-
-    date_id UUID NOT NULL REFERENCES dates(id) ON DELETE CASCADE,
+    date_id UUID NOT NULL REFERENCES dates(uuid) ON DELETE CASCADE,
+    UNIQUE (date_id, hour)
 );
