573eebdb8ccc4c7123a74ca72c92b87b1175a820
quangbach <167604957+quangbach27@users.noreply.github.com>
2025-06-17 16:28:10 +0700

    update debug mode for docker/app

--- docker/app/start.sh
+++ docker/app/start.sh
@@ -1,1 +1,16 @@
-go run .
+#!/bin/sh
+
+if [ "$DEBUG" = "true" ]; then
+  echo "⚙️Starting Go app at $PORT and (debug mode) at 40000..."
+  mkdir -p .bin
+
+  exec dlv debug ./ \
+    --headless \
+    --listen=:40000 \
+    --accept-multiclient \
+    --api-version=2 \
+    --log
+else
+  echo "🚀 Starting Go app at $PORT..."
+  exec go run .
+fi
